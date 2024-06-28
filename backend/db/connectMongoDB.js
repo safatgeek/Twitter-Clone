6f1775ae359a83c6f7efcab5ddead19253ffdb39
safatgeek <safatd310e@gmail.com>
2024-06-28 16:47:48 +0600

    First commit

---
+++ backend/db/connectMongoDB.js
@@ -0,0 +1,13 @@
+// @ts-nocheck
+import mongoose from "mongoose";
+ const connectMongoDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL)
+        console.log(`MongoDB connected: ${conn.connection.host}`)
+    } catch (error) {
+        console.error(`Error connection to mongoDB: ${error.message}`)
+        process.exit(1)
+    }
+ }
+
+ export default connectMongoDB
---
+++ backend/lib/utils/generateToken.js
@@ -0,0 +1,17 @@
+// @ts-nocheck
+import jwt from "jsonwebtoken"
+
+export const generateTokenAndSetCookie = (userId, res) => {
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+        expiresIn: "15d"
+    })
+
+    res.cookie("jwt", token, {
+        maxAge: 15*24*60*60*1000,
+        httpOnly: true,
+        sameSite: "strict",
+        source: process.env.NODE_ENV !== "development"
+
+
+    })
+}
