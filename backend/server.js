2845b784832be24f983df598e859a4daf1e4e17d
safatgeek <safatd310e@gmail.com>
2024-07-06 16:54:40 +0600

    Update API done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auths.routes.js"
 import userRoutes from "./routes/user.routes.js"
@@ -10,6 +11,12 @@ import connectMongoDB from "./db/connectMongoDB.js";
 
 
 dotenv.config()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+})
 
 const app = express()
 const PORT = process.env.PORT || 5000
