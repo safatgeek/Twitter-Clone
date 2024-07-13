dad78d6b6115dec185ff2568a9cbd018c759374e
safatgeek <safatd310e@gmail.com>
2024-07-13 18:32:23 +0600

    SignupPage done

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import React, { useState } from "react";
+import XSvg from "../../../components/svgs/X";
+
+import { Link } from "react-router-dom";
+
+import { MdOutlineMail } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+import { MdPassword } from "react-icons/md";
+import { MdDriveFileRenameOutline } from "react-icons/md";
 
 const SignUpPage = () => {
-  return (
-    <div>SignUpPage</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    fullname: "",
+    password: "",
+  });
 
-export default SignUpPage
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  return (
+    <div className="max-w-screen-xl mx-auto flex h-screen px-10">
+      <div className="flex-1 basis-1/2 hidden lg:flex items-center justify-center">
+        <XSvg className="lg:w-2/3 fill-white" />
+      </div>
+
+      <div className="flex-1 basis-1/4 flex flex-col justify-center items-center">
+        <form
+          className="lg:w-2/3 mx-auto md:mx-20 flex gap-4 flex-col"
+          onSubmit={handleSubmit}
+        >
+          <XSvg className="w-24 lg:hidden fill-white" />
+          <h1 className="text-4xl font-extrabold text-white">Join today</h1>
+          <label className="input input-bordered rounded flex items-center gap-2">
+            <MdOutlineMail />
+            <input
+              className="grow"
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={handleInputChange}
+              value={formData.email}
+            />
+          </label>
+
+          <label className="input input-bordered rounded flex items-center gap-2">
+            <FaUser />
+            <input
+              className="grow"
+              type="text"
+              name="username"
+              placeholder="Username"
+              onChange={handleInputChange}
+              value={formData.username}
+            />
+          </label>
+
+          <label className="input input-bordered rounded flex items-center gap-2">
+            <MdDriveFileRenameOutline />
+            <input
+              className="grow"
+              type="text"
+              name="fullname"
+              placeholder="Fullname"
+              onChange={handleInputChange}
+              value={formData.fullname}
+            />
+          </label>
+
+          <label className="input input-bordered rounded flex items-center gap-2">
+            <MdPassword />
+            <input
+              className="grow"
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={handleInputChange}
+              value={formData.password}
+            />
+          </label>
+
+          <button className="btn rounded-full btn-primary text-white btn-outline w-full">
+            Sign up
+          </button>
+        </form>
+
+        <div className="flex flex-col lg:w-2/3 gap-2 mt-4">
+          <p className="text-white text-lg">Already have an account?</p>
+          <Link to="/login">
+            <button className="btn rounded-full btn-primary text-white btn-outline">
+              Sign in
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
