bafd8d1f0479b5152b84ac5998801251be908759
safatgeek <safatd310e@gmail.com>
2024-08-29 17:23:31 +0600

    notifications & deleteNotifications API integrated

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -4,34 +4,62 @@ import { IoSettingsOutline } from "react-icons/io5";
 import { FaHeart, FaUser } from "react-icons/fa";
 import LoadingSpinner from "./../../components/common/LoadingSpinner";
 import { Link } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
 
 const NotificationPage = () => {
-  const isLoading = false;
-  const notificationsDummy = [
-    {
-      _id: "1",
-      from: {
-        _id: 1,
-        username: "monir",
-        profileImg: "/avatars/boy2.png",
-      },
-      type: "follow",
-    },
 
-    {
-      _id: "2",
-      from: {
-        _id: 2,
-        username: "dibbo",
-        profileImg: "/avatars/girl1.png",
-      },
-      type: "like",
-    },
-  ];
+  const queryClient = useQueryClient()
 
-  const deleteNotification = () => {
-    alert("All notifications deleted");
-  };
+  const {data:notification} = useQuery({
+    queryKey: ["notifications"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/notifications")
+
+        const data = await res.json()
+
+        if(!res.ok) {
+          throw new Error(data.error || "Something went wrong")
+        }
+
+        return data
+      } catch (error) {
+        throw new Error(error.message)
+      }
+    }
+
+  })
+ const {mutate: deleteNotification, isPending} = useMutation({
+  mutationFn: async () => {
+    try {
+      const res = await fetch("/api/notifications", {
+        method: "DELETE"
+      })
+
+      const data = await res.json()
+
+      if(!res.ok) {
+        throw new Error(data.error || "Something went wrong")
+      }
+
+      return data
+    } catch (error) {
+      throw new Error(error.message)
+    }
+  },
+
+  onSuccess: () => {
+    toast.success("Notifications deleted successfully")
+    queryClient.invalidateQueries({queryKey: ["notifications"]})
+  }
+ })
+
+ const handleDeleteNotifications = () => {
+  deleteNotification()
+ }
+
+
   return (
     <div className="flex-[4_4_0] border-l border-r border-gray-700 min-h-screen">
       <div className="flex justify-between items-center p-4 border-b border-gray-700">
@@ -45,24 +73,24 @@ const NotificationPage = () => {
             className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <a onClick={deleteNotification}>Delete all notifications</a>
+              <a onClick={handleDeleteNotifications}>Delete all notifications</a>
             </li>
           </ul>
         </div>
       </div>
 
-      {isLoading && (
+      {isPending && (
         <div className="flex justify-center h-full items-center">
           <LoadingSpinner size="lg" />
         </div>
       )}
 
-      {notificationsDummy?.length === 0 && (
+      {notification?.length === 0 && (
         <div className="text-center p-4 font-bold">No notifications</div>
       )}
 
-      {notificationsDummy?.map((notification) => (
-        <div className="border-b border-gray-700" key={notificationsDummy.
+      {notification?.map((notification) => (
+        <div className="border-b border-gray-700" key={notification.
 // @ts-ignore
         _id}>
           <div className="flex gap-2 p-4">
