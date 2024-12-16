7c3ed843d0449c82433ce66c84cfc5ac5458d83c
safatgeek <safatd310e@gmail.com>
2024-12-16 15:04:01 +0600

    import daisyUI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,7 @@
 // @ts-nocheck
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   content: [
     "./index.html",
@@ -8,9 +10,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [daisyui],
 
   daisyui: {
     themes: [
